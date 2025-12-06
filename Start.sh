c08b0f02330298cfde6fcebf645758c64553dacb
iankiprot-alt <iankiprot@gmail.com>
2025-12-06 22:39:39 +0300

    Update Start.sh

--- Start.sh
+++ Start.sh
@@ -1,1 +1,9 @@
 
+#!/bin/bash
+if [ ! -z "$AUTH_TAR_B64" ]; then
+    echo "Extracting AUTH_TAR_B64..."
+    echo $AUTH_TAR_B64 | base64 -d | tar xvfz - -C ./auth
+fi
+
+echo "Starting IanMegaBot..."
+node index.js
